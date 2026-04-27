21c047adbe25cda2d9d59a5970ffa25df98787d4
joselmin-lab <joselmin@gmail.com>
2026-04-27 17:40:19 -0400

    feat: Add simulated database for products (datos.js)

---
+++ js/datos.js
@@ -0,0 +1,185 @@
+// Base de datos simulada para los productos AMS Shocks
+const productosDB = {
+    // ==========================================
+    // BITUBO KATSUMI
+    // ==========================================
+    "ae92-delanteros": {
+        nombre: "Toyota Ae92 Delanteros",
+        categoria: "Bitubo Katsumi",
+        imagenes: [
+            "img/productos/ae92-delanteros-1.png",
+            "img/productos/ae92-delanteros-2.png",
+            "img/productos/ae92-delanteros-3.png"
+        ],
+        especificaciones: [
+            "Regulable en altura",
+            "Regulación de compresión",
+            "Regulación de rebote",
+            "Orings VITON",
+            "Reservorio remoto",
+            "100% reparable",
+            "100% regulable",
+            "6 Meses de garantía",
+            "Topmount rotulado",
+            "Bujes excentricos corrección de comba",
+            "Hecho en Bolivia"
+        ]
+    },
+    "ae92-traseros": {
+        nombre: "Toyota Ae92 Traseros",
+        categoria: "Bitubo Katsumi",
+        imagenes: [
+            "img/productos/ae92-traseros-1.png",
+            "img/productos/ae92-traseros-2.png",
+            "img/productos/ae92-traseros-3.png"
+        ],
+        especificaciones: [
+            "Regulable en altura",
+            "Regulación de compresión",
+            "Regulación de rebote",
+            "Orings VITON",
+            "Reservorio remoto",
+            "100% reparable",
+            "100% regulable",
+            "6 Meses de garantía",
+            "Topmount rotulado",
+            "Bujes excentricos corrección de comba",
+            "Hecho en Bolivia"
+        ]
+    },
+    "swift-delanteros": {
+        nombre: "Suzuki Swift Delanteros",
+        categoria: "Bitubo Katsumi",
+        imagenes: [
+            "img/productos/swift-delanteros-1.png",
+            "img/productos/swift-delanteros-2.png",
+            "img/productos/swift-delanteros-3.png"
+        ],
+        especificaciones: [
+            "Regulable en altura",
+            "Regulación de compresión",
+            "Regulación de rebote",
+            "Orings VITON",
+            "Reservorio remoto",
+            "100% reparable",
+            "100% regulable",
+            "6 Meses de garantía",
+            "Topmount rotulado",
+            "Bujes excentricos corrección de comba",
+            "Hecho en Bolivia"
+        ]
+    },
+    "g6-delanteros": {
+        nombre: "Toyota G6 Delanteros",
+        categoria: "Bitubo Katsumi",
+        imagenes: [
+            "img/productos/g6-delanteros-1.png",
+            "img/productos/g6-delanteros-2.png",
+            "img/productos/g6-delanteros-3.png"
+        ],
+        especificaciones: [
+            "Regulable en altura",
+            "Regulación de compresión",
+            "Regulación de rebote",
+            "Orings VITON",
+            "Reservorio remoto",
+            "100% reparable",
+            "100% regulable",
+            "6 Meses de garantía",
+            "Topmount rotulado",
+            "Bujes excentricos corrección de comba",
+            "Hecho en Bolivia"
+        ]
+    },
+    "g6-traseros": {
+        nombre: "Toyota G6 Traseros",
+        categoria: "Bitubo Katsumi",
+        imagenes: [
+            "img/productos/g6-traseros-1.png",
+            "img/productos/g6-traseros-2.png",
+            "img/productos/g6-traseros-3.png"
+        ],
+        especificaciones: [
+            "Regulable en altura",
+            "Regulación de compresión",
+            "Regulación de rebote",
+            "Orings VITON",
+            "Reservorio remoto",
+            "100% reparable",
+            "100% regulable",
+            "6 Meses de garantía",
+            "Topmount rotulado",
+            "Bujes excentricos corrección de comba",
+            "Hecho en Bolivia"
+        ]
+    },
+    "vitz-delanteros": {
+        nombre: "Toyota Vitz Delanteros",
+        categoria: "Bitubo Katsumi",
+        imagenes: [
+            "img/productos/vitz-delanteros-1.png",
+            "img/productos/vitz-delanteros-2.png",
+            "img/productos/vitz-delanteros-3.png"
+        ],
+        especificaciones: [
+            "Regulable en altura",
+            "Regulación de compresión",
+            "Regulación de rebote",
+            "Orings VITON",
+            "Reservorio remoto",
+            "100% reparable",
+            "100% regulable",
+            "6 Meses de garantía",
+            "Topmount rotulado",
+            "Bujes excentricos corrección de comba",
+            "Hecho en Bolivia"
+        ]
+    },
+    "wrx-delanteros": {
+        nombre: "Subaru Wrx Delanteros",
+        categoria: "Bitubo Katsumi",
+        imagenes: [
+            "img/productos/wrx-delanteros-1.png",
+            "img/productos/wrx-delanteros-2.png",
+            "img/productos/wrx-delanteros-3.png"
+        ],
+        especificaciones: [
+            "Regulable en altura",
+            "Regulación de compresión",
+            "Regulación de rebote",
+            "Orings VITON",
+            "Reservorio remoto",
+            "100% reparable",
+            "100% regulable",
+            "6 Meses de garantía",
+            "Topmount rotulado",
+            "Bujes excentricos corrección de comba",
+            "Hecho en Bolivia"
+        ]
+    },
+    "wrx-traseros": {
+        nombre: "Subaru Wrx Traseros",
+        categoria: "Bitubo Katsumi",
+        imagenes: [
+            "img/productos/wrx-traseros-1.png",
+            "img/productos/wrx-traseros-2.png",
+            "img/productos/wrx-traseros-3.png"
+        ],
+        especificaciones: [
+            "Regulable en altura",
+            "Regulación de compresión",
+            "Regulación de rebote",
+            "Orings VITON",
+            "Reservorio remoto",
+            "100% reparable",
+            "100% regulable",
+            "6 Meses de garantía",
+            "Topmount rotulado",
+            "Bujes excentricos corrección de comba",
+            "Hecho en Bolivia"
+        ]
+    }
+
+    // Nota: Aquí podrás agregar los Monotubo 2", Monotubo 2.5" y Coilovers en el futuro,
+    // copiando y pegando el mismo formato de arriba con sus respectivas especificaciones.
+};
