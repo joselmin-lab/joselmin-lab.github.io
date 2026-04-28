b27e5ebfbd96db838d2864b28c812efb69aa799b
joselmin-lab <joselmin@gmail.com>
2026-04-27 23:05:34 -0400

    Update G6 product entries with new variants

--- js/datos.js
+++ js/datos.js
@@ -66,7 +66,7 @@ const productosDB = {
             "Hecho en Bolivia"
         ]
     },
-    "g6-delanteros": {
+    "g6-delanteros-570": {
         nombre: "Toyota G6 Delanteros 570mm",
         categoria: "Bitubo Katsumi",
         imagenes: [
@@ -87,8 +87,51 @@ const productosDB = {
             "Hecho en Bolivia"
         ]
     },
-    "g6-traseros": {
+    "g6-traseros-650": {
         nombre: "Toyota G6 Traseros 650mm",
+        categoria: "Bitubo Katsumi",
+        imagenes: [
+            "img/productos/g6-traseros-1.png",
+            "img/productos/g6-traseros-2.png"
+        ],
+        especificaciones: [
+            "Regulable en altura",
+            "Regulación de compresión",
+            "Regulación de rebote",
+            "Orings VITON",
+            "Reservorio remoto",
+            "100% reparable",
+            "100% regulable",
+            "6 Meses de garantía",
+            "Topmount rotulado",
+            "Bujes excentricos corrección de comba",
+            "Hecho en Bolivia"
+        ]
+    },
+    
+    "g6-delanteros-640": {
+        nombre: "Toyota G6 Delanteros 640mm",
+        categoria: "Bitubo Katsumi",
+        imagenes: [
+            "img/productos/g6-delanteros-1.png",
+            "img/productos/g6-delanteros-2.png"
+        ],
+        especificaciones: [
+            "Regulable en altura",
+            "Regulación de compresión",
+            "Regulación de rebote",
+            "Orings VITON",
+            "Reservorio remoto",
+            "100% reparable",
+            "100% regulable",
+            "6 Meses de garantía",
+            "Topmount rotulado",
+            "Bujes excentricos corrección de comba",
+            "Hecho en Bolivia"
+        ]
+    },
+    "g6-traseros-690": {
+        nombre: "Toyota G6 Traseros 690mm",
         categoria: "Bitubo Katsumi",
         imagenes: [
             "img/productos/g6-traseros-1.png",
